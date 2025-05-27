96a326cb5f41912c12c428bb19fd383ac241f0fd
Zeongit <zeongit1995@gmail.com>
2025-05-27 16:15:07 +0800

    init

--- build/plugins/index.ts
+++ build/plugins/index.ts
@@ -1,11 +1,11 @@
 import type { PluginOption } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
+import ViteCompression from 'vite-plugin-compression';
 import VueDevTools from 'vite-plugin-vue-devtools';
 import { visualizer } from 'rollup-plugin-visualizer';
 import UnoCSS from 'unocss/vite';
 import unplugin from './unplugin';
-
 /**
  * vite插件
  *
@@ -23,10 +23,19 @@ export function setupVitePlugins(viteEnv: ImportMetaEnv): (PluginOption | Plugin
     //   enable: true // 只有开发环境才开启mock
     // }),
     visualizer({
-      open: true,
+      open: false,
       gzipSize: true, // 显示 gzip 之后的体积
       brotliSize: true // 显示 brotli 之后的体积
-    })
+    }),
+    ViteCompression({ algorithm: 'gzip' }),
+    // babel({
+    //   babelConfig: {
+    //     plugins: [
+    //       ['@babel/plugin-proposal-decorators', { legacy: true }],
+    //       ['@babel/plugin-proposal-class-properties', { loose: true }]
+    //     ]
+    //   }
+    // })
   ];
   return plugins;
 }
